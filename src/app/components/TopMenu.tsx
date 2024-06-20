26cdf2596ed0459bccffa53b77b9e0c5a5acce5a
Leo Thay <thaybunlong20@gmail.com>
2024-06-20 12:43:58 +0700

    feat: update UI for breadcrumb and article

--- src/app/components/TopMenu.tsx
+++ src/app/components/TopMenu.tsx
@@ -4,85 +4,83 @@ import Link from 'next/link';
 
 export default function TopMenu() {
   return (
-    <div>
-      <div className="md:flex">
-        {/* Logo */}
-        <Image
-          className="md:absolute object-contain mx-32"
-          width={100}
-          height={100}
-          src={'/dummy_300x300_ffffff_6074f2.png'}
-          alt=""
-        />
-        {/* Top Menu */}
-        <div className="w-full">
-          <nav className="flex my-6 mx-64">
-            <ul className="inline-flex items-center space-x-1 md:space-x-14 rtl:space-x-reverse">
-              <li>
-                <Link
-                  className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
-                  href="#"
-                >
-                  SHOP
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-[14px] tracking-wider font-medium focus:font-medium underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
-                  href="#"
-                >
-                  RECIPES
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
-                  href="#"
-                >
-                  LEARN
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
-                  href="#"
-                >
-                  ABOUT
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
-                  href="#"
-                >
-                  BLOG
-                </Link>
-              </li>
-            </ul>
-          </nav>
+    <header className="md:flex">
+      {/* Logo */}
+      <Image
+        className="md:absolute object-contain mx-32"
+        width={100}
+        height={100}
+        src={'/dummy_300x300_ffffff_6074f2.png'}
+        alt=""
+      />
+      {/* Top Menu */}
+      <section className="w-full">
+        <nav className="flex my-6 mx-64">
+          <ul className="inline-flex items-center space-x-1 md:space-x-14 rtl:space-x-reverse">
+            <li>
+              <Link
+                className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
+                href="#"
+              >
+                SHOP
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-[14px] tracking-wider font-medium focus:font-medium underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
+                href="#"
+              >
+                RECIPES
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
+                href="#"
+              >
+                LEARN
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
+                href="#"
+              >
+                ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-[14px] tracking-wider font-medium focus:font-medium focus:underline underline-offset-8 decoration-2 decoration-red-700 hover:underline hover:decoration-red-400"
+                href="#"
+              >
+                BLOG
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-          {/* Sub Menu */}
-          <nav className="flex h-12 my-7 px-64 bg-orange-50">
-            <ul className="inline-flex items-center space-x-1 md:space-x-8 rtl:space-x-reverse">
-              <li>
-                <Link className="text-xs tracking-wider font-medium" href="#">
-                  CATEGORIES
-                </Link>
-              </li>
-              <li>
-                <Link className="text-xs tracking-wider font-medium" href="#">
-                  COLLECTIONS
-                </Link>
-              </li>
-              <li>
-                <Link className="text-xs tracking-wider font-medium" href="#">
-                  RESOURCES
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+        {/* Sub Menu */}
+        <nav className="flex h-12 my-7 px-64 bg-orange-50">
+          <ul className="inline-flex items-center space-x-1 md:space-x-8 rtl:space-x-reverse">
+            <li>
+              <Link className="text-xs tracking-wider font-medium" href="#">
+                CATEGORIES
+              </Link>
+            </li>
+            <li>
+              <Link className="text-xs tracking-wider font-medium" href="#">
+                COLLECTIONS
+              </Link>
+            </li>
+            <li>
+              <Link className="text-xs tracking-wider font-medium" href="#">
+                RESOURCES
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,179 @@
 import React from 'react';
 import Link from 'next/link';
 import TopMenu from './components/TopMenu';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
       <TopMenu />
-      {/* Breadcrumbs */}
-      <nav className="flex" aria-label="breadcrumb">
-        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-          <li className="inline-flex items-center">
-            <Link
-              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-              href="/"
-            >
-              RECIPES
-              <svg
-                className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                  color="#C68194"
-                />
-              </svg>
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <Link
-              className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
-              href="/blog"
-            >
-              BREAD
-            </Link>
-            <svg
-              className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-                color="#C68194"
-              />
-            </svg>
-          </li>
-          <li className="flex items-center" aria-current="page">
-            <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
-              QUICK BREAD
-            </span>
-            <svg
-              className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-                color="#C68194"
-              />
-            </svg>
-          </li>
-        </ol>
-      </nav>
-
       {/* Article Content */}
-      <article>
-        <h1>Article Title</h1>
-        <div>
-          {/* Content Section */}
-          <section>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-            {/* Add more content here */}
-          </section>
+      <article className="flex mx-12 lg:ml-32 lg:mr-20">
+        <section className="w-[47%] pr-20">
+          {/* Breadcrumbs */}
+          <nav className="flex mt-5" aria-label="breadcrumb">
+            <ol className="inline-flex items-center">
+              <li className="inline-flex items-center">
+                <Link
+                  className="inline-flex items-center text-xs font-semibold"
+                  href="#"
+                >
+                  RECIPES
+                  <svg
+                    className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 9 4-4-4-4"
+                      color="#C68194"
+                    />
+                  </svg>
+                </Link>
+              </li>
+              <li className="flex items-center">
+                <Link
+                  className="inline-flex items-center text-xs font-semibold"
+                  href="#"
+                >
+                  BREAD
+                </Link>
+                <svg
+                  className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                    color="#C68194"
+                  />
+                </svg>
+              </li>
+              <li className="flex items-center" aria-current="page">
+                <span className="inline-flex items-center text-xs font-semibold">
+                  QUICK BREAD
+                </span>
+                <svg
+                  className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                    color="#C68194"
+                  />
+                </svg>
+              </li>
+            </ol>
+          </nav>
 
-          {/* Image Section */}
-          <section>
-            <img src="/article-image.jpg" alt="Article Image" />
-          </section>
-        </div>
+          <h1 className="text-4xl font-semibold mt-6">
+            Whole-Grain Banana Bread
+          </h1>
+          <div className="w-full mt-28">
+            {/* Content Section */}
+            <section>
+              <p>
+                This one-bowl banana bread — our 2018 Recipe of the Year — uses
+                the simplest ingredients, but is incredibly moist and flavorful.
+                While the recipe calls for a 50/50 mix of flours (all-purpose
+                and whole wheat), we often make the bread 100% whole wheat, and
+                honestly? No one can tell, it&apos;s that good! And not only is
+                this bread delicious — it&apos;s versatile.
+              </p>
+              {/* Add more content here */}
+            </section>
+            <section>
+              <div className="stats stats--recipe with-rule">
+                <div className="stat__item stat__item--prep">
+                  <div className="label">Prep</div>
+                  <span>10 mins</span>
+                </div>
+
+                <div className="stat__item stat__item--bake">
+                  <div className="label">Bake</div>
+                  <span>1 hr to 1 hr 15 mins</span>
+                </div>
+
+                <div className="stat__item stat__item--total">
+                  <div className="label">Total</div>
+                  <span>1 hr 25 mins</span>
+                </div>
+              </div>
+              <div className="stats stats--recipe stats--yield-row ">
+                <div className="stat__item stat__item--yield">
+                  <div className="label">Yield</div>
+                  <span>one 9&quot; x 5&quot; loaf</span>
+                </div>
+
+                <div className="stat__item stat__item--recipe-box">
+                  <a
+                    href="/recipebox"
+                    className="btn btn--save js-kaf-recipe-box-add"
+                    data-recipe-id="18916"
+                    data-recipe-uuid="cb4174f2-05cd-4032-8ce5-2f1d07315a1f"
+                    aria-label="Save Recipe: Whole Grain Banana Bread"
+                    data-once="recipe-box-add"
+                  >
+                    <svg
+                      enable-background="new 0 0 15 15"
+                      viewBox="0 0 15 15"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="m8 7v-7h-1v7h-7v1h7v7h1v-7h7v-1z"></path>
+                    </svg>
+                    Save Recipe
+                  </a>
+                </div>
+
+                <div className="stat__item stat__item--print">
+                  <button
+                    id="print-recipe-button"
+                    className="btn btn--print"
+                    data-node="18916"
+                  >
+                    <svg
+                      enable-background="new 0 0 21.1 20.1"
+                      viewBox="0 0 21.1 20.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="m20.6 3c-.3-.3-.7-.5-1.2-.5h-1.6v-2.5h-14.6v2.5h-1.5c-.5 0-.9.2-1.2.5s-.5.7-.5 1.2v8.3c0 .5.2.9.5 1.2s.7.5 1.2.5h2.3v5.9h13v-5.9h2.4c.5 0 .9-.2 1.2-.5s.5-.7.5-1.2v-8.3c0-.5-.2-.9-.5-1.2zm-3.6-2.1v1.6h-12.9v-1.6zm-13 7.4v4.9h-2.3c-.5 0-.8-.3-.8-.8v-8.2c0-.5.2-.7.8-.7h17.8c.5 0 .8.2.8.7v8.3c0 .5-.2.8-.8.8h-2.4v-5zm.9 10.9v-9.9h11.2v9.9z"></path>
+                    </svg>
+                    Print
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+
+        {/* Image Section */}
+        <section className="w-[55%]">
+          <Image
+            width={960}
+            height={636}
+            src="/article-image.jpg"
+            alt="Article Image"
+          />
+        </section>
       </article>
     </main>
   );
